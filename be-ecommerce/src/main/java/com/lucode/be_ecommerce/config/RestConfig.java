9d6acaa7f5d5a7052b043eaf7794a196bc0c0a3e
smile-dev49 <nancyspace89@gmail.com>
2015-02-20 13:00:51 +0200

    complete entities definition in spring

--- be-ecommerce/src/main/java/com/lucode/be_ecommerce/config/RestConfig.java
+++ be-ecommerce/src/main/java/com/lucode/be_ecommerce/config/RestConfig.java
@@ -1,5 +1,10 @@
 package com.lucode.be_ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,14 +14,29 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.lucode.be_ecommerce.entity.Category;
 import com.lucode.be_ecommerce.entity.Product;
 
-@Configuration
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;;
+
+@Configuration // Indicates that this class contains configuration settings
 public class RestConfig implements RepositoryRestConfigurer {
 
+    // EntityManager allows interaction with the persistence context
+    private EntityManager entityManager;
+
+    // Constructor to inject the EntityManager dependency
+    // @Autowired
+    public RestConfig(EntityManager _entityManager){
+        entityManager = _entityManager;
+    }
+
+     // Override method to configure repository REST settings
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
+        // Call the parent method
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
 
+        // Define the HTTP methods that are not supported (disabled)
         HttpMethod[] unsupportedAction = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         // Disable for Product API
@@ -30,6 +50,32 @@ public class RestConfig implements RepositoryRestConfigurer {
             .forDomainType(Category.class)
             .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedAction))
             .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedAction));
+
+
+        // Call method to expose entity IDs in the API responses
+        exposeIds(config);
+    }
+
+    // Method to expose the IDs of entities in the API responses
+    private void exposeIds(RepositoryRestConfiguration config){
+
+        // Get all entities managed by the EntityManager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // Create a list to hold the entity classes
+        List<Class> entityClasses = new ArrayList<>();
+
+
+        // Add each entity's class to the list
+        for(EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // Convert the list to an array of classes
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+
+        // Expose the IDs for the specified domain types
+        config.exposeIdsFor(domainTypes);
     }
 
     
---
+++ fe-ecommerce/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    BaseURL : 'http://localhost:8080/api/products'
+};
