6f188bf12ab4022c7e14b61e267d72a42237f43c
smile-dev49 <nancyspace89@gmail.com>
2015-02-27 13:07:40 +0200

    implement custom paginate

--- fe-ecommerce/src/app/components/product-list/product-list.component.ts
+++ fe-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product/product.service';
 import { Product } from '../../common/product';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, NgClass } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { debounce, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [CurrencyPipe, RouterLink],
+  imports: [CurrencyPipe, RouterLink, NgClass],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
@@ -15,6 +14,11 @@ export class ProductListComponent implements OnInit{
 
   products!:Product[];
   currentCategoryId!: number | null;
+
+  pageNumber: number = 0;
+  pageSize: number = 10;
+  totalElements: number = 0;
+  totalPage : number = 1;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -26,6 +30,21 @@ export class ProductListComponent implements OnInit{
         this.listProducts();
       }
     );
+  }
+
+  handlePaginate(page : any){
+    if (page == 'prev' && this.pageNumber > 0) {
+      this.pageNumber--;
+      this.listProducts();
+    }
+    else if(page == 'next' && this.pageNumber < this.totalPage - 1){
+      this.pageNumber++;
+      this.listProducts();
+    }
+    else{
+      this.pageNumber = page;
+      this.listProducts();
+    }
   }
 
   listProducts(){
@@ -43,14 +62,21 @@ export class ProductListComponent implements OnInit{
   }
 
   handleListProducts(){
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      products => this.products = products
-    )
+    this.productService.getProductListPaginate( this.pageNumber,
+                                                this.pageSize,
+                                                this.currentCategoryId)
+                                                .subscribe(
+                                                  response => {
+                                                    this.products = response._embedded.products;
+                                                    this.pageNumber = response.page.number;
+                                                    this.pageSize = response.page.size;
+                                                    this.totalElements = response.page.totalElements;
+                                                    this.totalPage = response.page.totalPages;
+                                                  })
   }
 
   handleSearchProducts(){
     const theKeyWord: string|null = this.route.snapshot.paramMap.get('keyword');
-    console.log(theKeyWord);
     
     this.productService.getProductSearchList(theKeyWord)
     .subscribe(
--- fe-ecommerce/src/app/pages/detail-product/detail-product.component.ts
+++ fe-ecommerce/src/app/pages/detail-product/detail-product.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product/product.service';
+import { ActivatedRoute, Router, RouterLinkActive } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-detail-product',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './detail-product.component.html',
   styleUrl: './detail-product.component.scss'
 })
-export class DetailProductComponent {
+export class DetailProductComponent implements OnInit {
 
+  public product!: Product;
+
+  constructor(private productService : ProductService,
+              private route : ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+
+    this.handleProduct(productId);
+  }
+
+  handleProduct(id : string | null): void  {
+    this.productService.getProduct(id).subscribe(
+      product => this.product = product
+    );
+  }
+  
 }
