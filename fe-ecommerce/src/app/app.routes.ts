9ba5af0603a85a44a1d501773911bb30ef22b4e3
Lucode710 <luca.venturi.710@gmail.com>
2024-05-25 23:24:34 +0200

    implement routerLink in sidebar

--- fe-ecommerce/src/app/app.routes.ts
+++ fe-ecommerce/src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    // {path: 'category/:id', co}
+    {path: 'category/:id', component: HomeComponent},
+    {path: 'category', component: HomeComponent},
+    {path: 'products', component: HomeComponent},
+    {path: '', redirectTo: '/products', pathMatch: 'full'},
+    {path: '**', redirectTo: '/products', pathMatch: 'full'}
 ];
